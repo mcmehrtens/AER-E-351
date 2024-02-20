cfb87dfa3eb58e119b35367685ec094a8aa0a58f
Matthew Mehrtens <matthew@mehrtens.cc>
2024-02-19 22:59:02 -0600

    Corrected hw2 problem 2.6d

--- Homework 02/Problem_2_6_d.m
+++ Homework 02/Problem_2_6_d.m
@@ -7,10 +7,13 @@ R_Earth = 6.37812e3; % [km]
 mu_Earth = 3.986e5; % [km^3/s^2]
 
 %% Calculations
-delta_t = sqrt(288 * R_Earth^3 / mu_Earth); % [s]
+R = 144 * R_Earth; % [km]
+delta_t = sqrt(2 / 9 * R / mu_Earth) * R; % [s]
 delta_t_days = delta_t / 3600 / 24; % [days]
+total_time = delta_t_days * 2; % [days]
 
 fprintf( ...
     "delta_t = %g s\n" + ...
-    "delta_t = %g days\n", ...
-    delta_t, delta_t_days);
+    "delta_t = %g days\n" + ...
+    "total_time = %g days\n", ...
+    delta_t, delta_t_days, total_time);
